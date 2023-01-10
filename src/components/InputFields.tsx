2611ac978c52a37a44d5803c050bcf172a25b7cd
Ogaga <iyaraogaga69@gmail.com>
2023-01-10 04:01:45 +0100

    Add interface for todo

--- src/components/InputFields.tsx
+++ src/components/InputFields.tsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./styles.css";
 
 interface Props {
   todo: string;
   setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAdd: (e: React.FormEvent) => void;
 }
 
-const InputFields: React.FC<Props> = ({ todo, setTodo }) => {
+const InputFields: React.FC<Props> = ({ todo, setTodo, handleAdd }) => {
+    const inputRef = useRef<HTMLInputElement>(null);
+
   return (
-    <form className="input">
-      <input
+      <form className="input" onSubmit={(e) => {
+          handleAdd(e)
+          inputRef.current?.blur()
+      }}>
+          <input
+              ref={inputRef}
         type="input"
         value={todo}
         onChange={(e) => {
---
+++ src/components/TodoList.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Todo } from '../model';
+import './styles.css'
+
+interface Props {
+  todos: Todo[];
+  setTodos: React.Dispatch<React.SetStateAction<Todo[]>>;
+}
+
+const TodoList: React.FC<Props> = ({todos, setTodos}) => {
+    return (
+        <div className='todos'>
+            
+        </div>
+    );
+};
+
+export default TodoList;
